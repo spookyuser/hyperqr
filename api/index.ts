f9cf947e14c20a91c79baadc699e1f5bed0f8127
spookyuser <spookyuser@users.noreply.github.com>
2023-08-12 21:48:52 +0200

    Switch to vercel bc yeah

---
+++ api/index.ts
@@ -0,0 +1,55 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const config = {
+  runtime: "edge",
+};
+
+const app = new Hono();
+
+import QRCode from "qrcode";
+// With async/await
+const generateQR = async (data: string) => {
+  try {
+    return QRCode.toString(data, { type: "svg" });
+  } catch (err) {
+    console.error(err);
+  }
+};
+// With promises
+
+const htmlqr = (qrsvg: string) => `
+<!DOCTYPE html>
+ <html>
+    <head>
+      <title>QR Code Generator</title>
+      <style>
+        body {
+          display: flex;
+          justify-content: center; 
+          align-items: center;     
+          margin: 0;               
+        }
+        svg {
+          max-width: 80vh;
+        }
+      </style>
+    </head>
+    <body>
+      ${qrsvg}
+    </body>
+  </html>
+`;
+
+app.get("*", async (c) => {
+  let data = c.req.path.substring(1);
+  const qr = await generateQR(data);
+  console.log(data);
+
+  if (qr) {
+    return c.html(htmlqr(qr));
+  }
+  return c.html("Error");
+});
+
+export default handle(app);
