61ed3dccc0b874573010f289a1be30bdb2559c2b
spookyuser <spookyuser@users.noreply.github.com>
2023-08-13 14:36:01 +0200

    Add wasm-pack as a dependency?

--- qrlib/install.sh
+++ qrlib/install.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y 
-source $HOME/.cargo/env 
-cargo install wasm-pack 
-rustup install stable
-rustup target add wasm32-unknown-unknown
+if [ "$VERCEL_ENV" != "production" ]; then
+  echo "This script is for installing on vercel 💁‍♀️"
+  exit 1
+fi
+
+# Exit if wasm-pack is already installed
+if [ -x "$(command -v wasm-pack)" ]; then
+  echo "wasm-pack is already installed"
+  exit 0
+fi
+
+pnm install -g wasm-pack
