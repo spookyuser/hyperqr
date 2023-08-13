2ab08d28cbfec068d81e266696d70d5b60201c2a
spookyuser <spookyuser@users.noreply.github.com>
2023-08-13 15:22:32 +0200

    Wasm

---
+++ qrlib/install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+if [ -n "$VERCEL" ]; then
+
+    if ! command -v rustup &>/dev/null; then
+        echo "Rustup is not installed. Installing Rustup..."
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        rustup target add wasm32-unknown-unknown
+    fi
+
+    if ! command -v wasm-pack &>/dev/null; then
+        echo "wasm-pack is not installed. Installing wasm-pack..."
+        curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -f
+    fi
+
+    echo "Updating all installed packages..."
+    cargo install-update -a
+
+fi
